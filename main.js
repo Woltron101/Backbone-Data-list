795b768a34e08f46f04166fcf7cd71ab6c4a45fe
woltron101 <woltron101@gmail.com>
2017-02-06 22:49:26 +0200

    format

--- main.js
+++ main.js
@@ -18,45 +18,24 @@ $(function() {
         search: function() {
             var text = this.$el.val();
             app.views.Select.search = text;
-            console.log("app.views.Select.search ", app.views.Select.search);
             return text;
-
         },
     })
-
-
-    app.models.Option = Backbone.Model.extend({})
-    app.views.Option = Backbone.View.extend({
-        tagName: 'option',
-
-        template: template('optionTemplate'),
-        render: function() {
-
-
-
-            this.$el.html(this.template(this.model));
-            return this;
-        },
-    })
-
 
     app.models.ListItem = Backbone.Model.extend({})
     app.views.ListItem = Backbone.View.extend({
         tagName: 'li',
 
         template: template('listIteTemplate'),
+
         render: function() {
-
-
-
             this.$el.html(this.template(this.model));
             return this;
-        },
+        }
     })
 
     app.collections.Select = Backbone.Collection.extend({
-        model: app.models.Option,
-
+        model: app.models.Option
     });
 
     app.views.Select = Backbone.View.extend({
@@ -67,31 +46,21 @@ $(function() {
             _.bindAll(this, 'on_keypress');
             $(document).bind('keydown', this.on_keypress);
         },
-        render: function() {
-            this.$el.find('select').html('');
-            var collection = _.filter(this.collection.toJSON(), this.filter)
-            collection = _.sortBy(collection, 'firstName', this)
-            collection.forEach(this.addOption, this);
-            return this;
-        },
 
-        renderList: function() {
+        render: function() {
             this.toggleList();
             this.$el.find('ul.dropdown-list').html('');
-            var collection = _.filter(this.collection.toJSON(), this.filter)
+            var collection = _.filter(this.collection.toJSON(), this.filter);
             collection = _.sortBy(collection, 'firstName', this)
             collection.forEach(this.addListItem, this);
             return this;
         },
         events: {
-            'input #search': function() {
-                // this.render();
-                this.renderList();
-            },
+            'input #search': 'render',
             'click .dropdown-list li': 'selectValue',
             'click .emails li span': 'removeEmail'
-
         },
+
         filter: function(obj) {
             var text = new RegExp($('#search').val(), 'i');
             for (key in obj) {
@@ -99,10 +68,13 @@ $(function() {
             }
             return false;
         },
+
         toggleList: function() {
             if ($('input').val()) $('.dropdown-list').removeAttr('hidden');
             else $('.dropdown-list').attr('hidden', true);
+            return this;
         },
+
         on_keypress: function(e) {
             if (e.keyCode == 40) {
                 $('.dropdown-list li.active').removeClass('active').next().addClass('active');
@@ -114,36 +86,45 @@ $(function() {
                 var thisLi = $('.dropdown-list li.active');
                 this.addMail(thisLi.text(), thisLi);
             }
+            return this;
         },
+
         selectValue: function(e) {
-            var thisLi = $(e.target).closest('li')
+            var thisLi = $(e.target).closest('li');
             $('.dropdown-list li.active').removeClass('active');
             this.addMail(thisLi.text(), thisLi);
+            return this;
         },
+
         addMail: function(credentials, li) {
             var lastSpace = credentials.lastIndexOf(' '),
                 mail = credentials.slice(lastSpace);
             $('.emails').append('<li>' + mail + '<span>×</span></ li>');
-            li.next().addClass('active')
+            li.next().addClass('active');
             li.remove();
+            return this;
         },
+
         removeEmail: function(e) {
             li = $(e.target).closest('li')
             li.remove();
-            item = _.find(this.collection.toJSON(), function(item) {
-                return item.email === li.text().replace('×', '').trim()
-            }, this)
-            this.addListItem(item)
+            this.moveToDropDown(li);
+            return this;
         },
-        addOption: function(opt) {
-            var option = new app.views.Option({ model: opt });
-            this.$el.find('select').append(option.render().el);
+
+        moveToDropDown: function(li) {
+            var item = _.find(this.collection.toJSON(), function(item) {
+                return item.email === li.text().replace('×', '').trim();
+            }, this);
+            this.addListItem(item);
+            return this;
         },
+
         addListItem: function(item, index) {
             var li = $('<li>'),
                 value = $('#search').val(),
-                regExp = new RegExp(strong, 'i'),
                 strong = value.charAt(0).toUpperCase() + value.slice(1),
+                regExp = new RegExp(strong, 'i'),
                 str = '';
 
             if (index > 4) return;
@@ -168,11 +149,13 @@ $(function() {
             }
             if (!$('.dropdown-list .active').length) li.addClass('active');
             this.$el.find('ul.dropdown-list').append(li.append(str));
+            return this;
         }
     })
+
     $.getJSON("data.json").success(function(data) {
         app.selectCollection = new app.collections.Select(data);
         app.selectView = new app.views.Select({ collection: app.selectCollection });
-        $('.select').append(app.selectView.renderList());
+        $('.select').append(app.selectView.render());
     })
 })
